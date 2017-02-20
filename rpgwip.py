cc371a4d2e0e35f147da5c3a4ae92d915a215ebe
calebbernard <calebbernard@gmail.com>
2017-02-20 07:02:58 -0600

    Update rpgwip.py

--- rpgwip.py
+++ rpgwip.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python
 
+#notes: maybe leave formulas up to init.txt
+
 import libtcodpy as libtcod
 import math
 import textwrap
@@ -7,44 +9,27 @@ import shelve
 
 
 #actual size of the window
-SCREEN_WIDTH = 30
-SCREEN_HEIGHT = 30
+SCREEN_WIDTH = 15
+SCREEN_HEIGHT = 12
 
 #size of the map
 MAP_WIDTH = 15
 MAP_HEIGHT = 12
 
-#sizes and coordinates relevant for the GUI
-BAR_WIDTH = 5
-PANEL_HEIGHT = 1
-PANEL_Y = SCREEN_HEIGHT - PANEL_HEIGHT
-MSG_X = BAR_WIDTH + 2
-MSG_WIDTH = SCREEN_WIDTH - BAR_WIDTH - 2
-MSG_HEIGHT = PANEL_HEIGHT - 1
-INVENTORY_WIDTH = 10
-CHARACTER_SCREEN_WIDTH = 10
-LEVEL_SCREEN_WIDTH = 10
-
 LIMIT_FPS = 20  #20 frames-per-second maximum
 
-color_dark_wall = libtcod.Color(0, 0, 100)
-color_light_wall = libtcod.Color(130, 110, 50)
-color_dark_ground = libtcod.Color(50, 50, 150)
-color_light_ground = libtcod.Color(200, 180, 50)
-
 class Map:
-    def __init__(self, width, height, tiles):
+    def __init__(self, width, height, tiles, warps):
         self.width = width
         self.height = height
         self.tiles = tiles
+        self.warps = warps
 
     def draw(self):
-        print self.tiles[9][0]
         for x in range(self.width):
             for y in range(self.height):
                 libtcod.console_set_default_foreground(con, self.tiles[x][y].color)
                 libtcod.console_put_char(con, x, y, self.tiles[x][y].char, libtcod.BKGND_NONE)
-        libtcod.console_blit(con, 0, 0, SCREEN_WIDTH, SCREEN_HEIGHT, 0, 0, 0)
 
 class Tile:
     #a tile of the map and its properties
@@ -53,6 +38,14 @@ class Tile:
         self.char = char
         self.color = libtcod.Color(red, green, blue)
         self.blocking = blocking
+
+class Warp:
+    def __init__(self, dest, origin_x, origin_y, dest_x, dest_y):
+        self.dest = dest
+        self.origin_x = origin_x
+        self.origin_y = origin_y
+        self.dest_x = dest_x
+        self.dest_y = dest_y
 
 class Object:
     #this is a generic object: the player, a monster, an item, the stairs...
@@ -67,9 +60,10 @@ class Object:
 
     def move(self, dx, dy):
         #move by the given amount, if the destination is not blocked
-        if not is_blocked(self.x + dx, self.y + dy):
-            self.x += dx
-            self.y += dy
+        if not oob(self.x + dx, self.y + dy):
+            if not is_blocked(self.x + dx, self.y + dy):
+                self.x += dx
+                self.y += dy
 
     def send_to_back(self):
         #make this object be drawn first, so all others appear above it if they're in the same tile.
@@ -78,12 +72,26 @@ class Object:
         objects.insert(0, self)
 
     def draw(self):
-        #only show if it's visible to the player; or it's set to "always visible" and on an explored tile
-        if (libtcod.map_is_in_fov(fov_map, self.x, self.y) or
-                (self.always_visible and map[self.x][self.y].explored)):
-            #set the color and then draw the character that represents this object at its position
-            libtcod.console_set_default_foreground(con, self.color)
-            libtcod.console_put_char(con, self.x, self.y, self.char, libtcod.BKGND_NONE)
+        #set the color and then draw the character that represents this object at its position
+        libtcod.console_set_default_foreground(con, self.color)
+        libtcod.console_put_char(con, self.x, self.y, self.char, libtcod.BKGND_NONE)
+
+    def warp(self):
+        dest = ""
+        dest_x = 0
+        dest_y = 0
+        warp = False
+        for item in map.warps:
+            if item.origin_x == self.x and item.origin_y == self.y:
+                dest = item.dest
+                dest_x = item.dest_x
+                dest_y = item.dest_y
+                warp = True
+        if warp:
+            clearscreen()
+            parsemap(dest)
+            self.x = dest_x
+            self.y = dest_y
 
     def clear(self):
         #erase the character that represents this object
@@ -91,149 +99,46 @@ class Object:
 
 def is_blocked(x, y):
     #first test the map tile
-    if map[x][y].blocked:
+    if map.tiles[x][y].blocking:
         return True
-
-    #now check for any blocking objects
-    for object in objects:
-        if object.blocks and object.x == x and object.y == y:
-            return True
-
     return False
 
-def render_bar(x, y, total_width, name, value, maximum, bar_color, back_color):
-    #render a bar (HP, experience, etc). first calculate the width of the bar
-    bar_width = int(float(value) / maximum * total_width)
-
-    #render the background first
-    libtcod.console_set_default_background(panel, back_color)
-    libtcod.console_rect(panel, x, y, total_width, 1, False, libtcod.BKGND_SCREEN)
-
-    #now render the bar on top
-    libtcod.console_set_default_background(panel, bar_color)
-    if bar_width > 0:
-        libtcod.console_rect(panel, x, y, bar_width, 1, False, libtcod.BKGND_SCREEN)
-
-    #finally, some centered text with the values
-    libtcod.console_set_default_foreground(panel, libtcod.white)
-    libtcod.console_print_ex(panel, x + total_width / 2, y, libtcod.BKGND_NONE, libtcod.CENTER,
-                                 name + ': ' + str(value) + '/' + str(maximum))
+def oob(x, y):
+    if y < 0 or y >= map.height or x < 0 or x >= map.width:
+        return True
+    return False
 
 def render_all():
-    global map
-    print "here"
-    libtcod.console_wait_for_keypress(True)
+    global map, player
+    player.clear()
     map.draw()
-
-def menu(header, options, width):
-    if len(options) > 26: raise ValueError('Cannot have a menu with more than 26 options.')
-
-    #calculate total height for the header (after auto-wrap) and one line per option
-    header_height = libtcod.console_get_height_rect(con, 0, 0, width, SCREEN_HEIGHT, header)
-    if header == '':
-        header_height = 0
-    height = len(options) + header_height
-
-    #create an off-screen console that represents the menu's window
-    window = libtcod.console_new(width, height)
-
-    #print the header, with auto-wrap
-    libtcod.console_set_default_foreground(window, libtcod.white)
-    libtcod.console_print_rect_ex(window, 0, 0, width, height, libtcod.BKGND_NONE, libtcod.LEFT, header)
-
-    #print all the options
-    y = header_height
-    letter_index = ord('a')
-    for option_text in options:
-        text = '(' + chr(letter_index) + ') ' + option_text
-        libtcod.console_print_ex(window, 0, y, libtcod.BKGND_NONE, libtcod.LEFT, text)
-        y += 1
-        letter_index += 1
-
-    #blit the contents of "window" to the root console
-    x = SCREEN_WIDTH/2 - width/2
-    y = SCREEN_HEIGHT/2 - height/2
-    libtcod.console_blit(window, 0, 0, width, height, 0, x, y, 1.0, 0.7)
-
-    #present the root console to the player and wait for a key-press
-    libtcod.console_flush()
-    key = libtcod.console_wait_for_keypress(True)
-
-    if key.vk == libtcod.KEY_ENTER and key.lalt:  #(special case) Alt+Enter: toggle fullscreen
-        libtcod.console_set_fullscreen(not libtcod.console_is_fullscreen)
-
-    #convert the ASCII code to an index; if it corresponds to an option, return it
-    index = key.c - ord('a')
-    if index >= 0 and index < len(options): return index
-    return None
-
-def msgbox(text, width=50):
-    menu(text, [], width)  #use menu() as a sort of "message box"
+    player.draw()
+    libtcod.console_blit(con, 0, 0, MAP_WIDTH, MAP_HEIGHT, 0, 0, 0)
 
 def handle_keys():
-    global key
-
-    if key.vk == libtcod.KEY_ENTER and key.lalt:
-        #Alt+Enter: toggle fullscreen
-        libtcod.console_set_fullscreen(not libtcod.console_is_fullscreen())
-
-    elif key.vk == libtcod.KEY_ESCAPE:
-        return 'exit'  #exit game
-
+    key = libtcod.console_wait_for_keypress(True)
+    if key.vk == libtcod.KEY_ESCAPE:
+        return True  #exit game
     if game_state == 'playing':
-        #movement keys
-        if key.vk == libtcod.KEY_UP or key.vk == libtcod.KEY_KP8:
-            player_move_or_attack(0, -1)
-        elif key.vk == libtcod.KEY_DOWN or key.vk == libtcod.KEY_KP2:
-            player_move_or_attack(0, 1)
-        elif key.vk == libtcod.KEY_LEFT or key.vk == libtcod.KEY_KP4:
-            player_move_or_attack(-1, 0)
-        elif key.vk == libtcod.KEY_RIGHT or key.vk == libtcod.KEY_KP6:
-            player_move_or_attack(1, 0)
-        else:
-            #test for other keys
-            key_char = chr(key.c)
+        if key.vk == libtcod.KEY_UP:
+            player.move(0, -1)
+        elif key.vk == libtcod.KEY_DOWN:
+            player.move(0, 1)
+        elif key.vk == libtcod.KEY_LEFT:
+            player.move(-1, 0)
+        elif key.vk == libtcod.KEY_RIGHT:
+            player.move(1, 0)
+        elif chr(key.c) == '.':
+            player.warp()
 
-            if key_char == '<':
-                #go down stairs, if the player is on them
-                if stairs.x == player.x and stairs.y == player.y:
-                    next_level()
-
-            return 'didnt-take-turn'
-
-def save_game():
-    #open a new empty shelve (possibly overwriting an old one) to write the game data
-    file = shelve.open('savegame', 'n')
-    file['map'] = map
-    file['objects'] = objects
-    file['player_index'] = objects.index(player)  #index of player in objects list
-    file['stairs_index'] = objects.index(stairs)  #same for the stairs
-    file['inventory'] = inventory
-    file['game_msgs'] = game_msgs
-    file['game_state'] = game_state
-    file['dungeon_level'] = dungeon_level
-    file.close()
-
-def load_game():
-    #open the previously saved shelve and load the game data
-    global map, objects, player, stairs, inventory, game_msgs, game_state, dungeon_level
-
-    file = shelve.open('savegame', 'r')
-    map = file['map']
-    objects = file['objects']
-    player = objects[file['player_index']]  #get index of player in objects list and access it
-    stairs = objects[file['stairs_index']]  #same for the stairs
-    inventory = file['inventory']
-    game_msgs = file['game_msgs']
-    game_state = file['game_state']
-    dungeon_level = file['dungeon_level']
-    file.close()
-
-    initialize_fov()
+def clearscreen():
+    for x in range(SCREEN_WIDTH):
+        for y in range(SCREEN_HEIGHT):
+            libtcod.console_put_char(con, x, y, ' ', libtcod.BKGND_NONE)
 
 def parsemap(file):
     global map
-    f = open (file, "r")
+    f = open (("./data/maps/" + file), "r")
     width = int(f.readline())
     height = int(f.readline())
     f.readline()
@@ -257,12 +162,11 @@ def parsemap(file):
                     # adds that block multiple times
                     expanded.append(element_cleaned)
         expanded_2d.append(expanded)
-    print expanded_2d
 
     for x in range(width):
         tilerow = []
         for y in range(height):
-            path = "data/tiles/" + expanded_2d[y][x] + ".txt"
+            path = "./data/tiles/" + expanded_2d[y][x] + ".txt"
             f2 = open(path, "r")
             char = (f2.readline())[0]
             red = int(f2.readline())
@@ -277,20 +181,29 @@ def parsemap(file):
             tile = Tile(expanded_2d[y][x], char, red, green, blue, blocking)
             tilerow.append(tile)
         tiles.append(tilerow)
-    for y in range(height):
-        for x in range(width):
-            print tiles[x][y].name
+
+    f.readline()
+    warps = []
+    numWarps = int(f.readline())
+    for x in range(numWarps):
+        z = (f.readline()).split()
+        warp = Warp(z[0], int(z[2]), int(z[3]), int(z[5]), int(z[6]))
+        print z[2]
+        print z[3]
+        warps.append(warp)
+
+
     f.close()
-    map = Map(width, height, tiles)
+    map = Map(width, height, tiles, warps)
 
 def new_game():
     global player, inventory, party, game_state, map
 
-    parsemap("data/maps/map.txt")
+    parsemap("map.txt")
     #libtcod.console_wait_for_keypress(True)
     #exit
     #create object representing the player
-    player = Object(0, 0, '@', 'player', libtcod.white, blocks=True)
+    player = Object(3, 5, '@', 'player', libtcod.Color(255,0,0), blocks=True)
 
     game_state = 'playing'
     inventory = []
@@ -301,36 +214,14 @@ def play_game():
         #render the screen
         render_all()
         libtcod.console_flush()
-
-
-def main_menu():
-    while not libtcod.console_is_window_closed():
-
-        #show the game's title, and some credits!
-        libtcod.console_set_default_foreground(0, libtcod.light_yellow)
-        libtcod.console_print_ex(0, SCREEN_WIDTH/2, SCREEN_HEIGHT/2-4, libtcod.BKGND_NONE, libtcod.CENTER,
-                                 'RPG')
-        libtcod.console_print_ex(0, SCREEN_WIDTH/2, SCREEN_HEIGHT-2, libtcod.BKGND_NONE, libtcod.CENTER, 'By Caleb')
-
-        #show options and wait for the player's choice
-        choice = menu('', ['Play a new game', 'Continue last game', 'Quit'], 24)
-
-        if choice == 0:  #new game
-            new_game()
-            play_game()
-        if choice == 1:  #load last game
-            try:
-                load_game()
-            except:
-                msgbox('\n No saved game to load.\n', 24)
-                continue
-            play_game()
-        elif choice == 2:  #quit
+        exit = handle_keys()
+        if exit:
             break
 
 libtcod.console_set_custom_font('arial12x12.png', libtcod.FONT_TYPE_GREYSCALE | libtcod.FONT_LAYOUT_TCOD)
 libtcod.console_init_root(SCREEN_WIDTH, SCREEN_HEIGHT, 'RPG', False)
 libtcod.sys_set_fps(LIMIT_FPS)
 con = libtcod.console_new(MAP_WIDTH, MAP_HEIGHT)
-panel = libtcod.console_new(SCREEN_WIDTH, PANEL_HEIGHT)
-main_menu()
+#main_menu()
+new_game()
+play_game()
