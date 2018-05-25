14f4fa9088986c4b3446db24f625b3f945743b4d
calebbernard <calebbernard@gmail.com>
2018-05-25 12:40:26 -0400

    Playing around w it

--- obfuscations/fizzbuzz.c
+++ obfuscations/fizzbuzz.c
@@ -1,15 +1,14 @@
 // Caleb Bernard 2018
-#include <stdio.h>
-main(int a){
+main(int a) {
 	unsigned char b = 'I', c = 'i', d = !a;
-	for (int e = '+.'; e != 'H<'; e = e >> 1 | ((e >> 0 ^ e >> 2 ^ e >> 3 ^ e >> 5) & 1) << 0xF) {
-		if (!d && c == 'i') c = c << 1 | c >> 7, d ^= 1;
-		else if (d && c == 'i') d ^= 1;
-		d ? c = c << 1 | c >> 7 : a;
-		if (c & 1) printf("%d\n", a);
-		else if (b == 'R') printf("FizzBuzz\n"), b = b << 2 | b >> 6;
-        else (b = b << 1 | b >> 7) & 1 ? printf("Buzz\n") : printf("Fizz\n");
-		!d ? c = c >> 1 | c << 7 : a;
-        a ^= a&~-~a|~a&-~a;
-	}
+	for (int e = 'yU'; e != 'r-' &
+		(((c == 'i' && ((d && !(d ^= 1)) 
+		|| (c = c << 1 | c >> 7, d ^= 1)))
+		| (d ? c = c << 1 | c >> 7 : a))
+		| (((((c & 1 && printf("%d\n", a)) 
+		|| (b == 'R' && puts("FizzBuzz") && (b = b << 2 | b >> 6)) 
+		|| ((b = b << 1 | b >> 7) & 1 ? puts("Buzz") : puts("Fizz")))
+		| (!d ? c = c >> 1 | c << 7 : a))
+		| (a ^= a&~-~a|~a&-~a)))); 
+		e = e >> 1 | ((e >> 0 ^ e >> 2 ^ e >> 3 ^ e >> 5) & 1) << 0xF);
 }
